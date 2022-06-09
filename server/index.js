c14d1f6323a84273f41884d9cd8c021cd0768bf6
lindokuhle <mrnvm8@gmail.com>
2022-06-09 16:48:04 +0200

    Update index.js
    change password

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: 'Lindo.1.',
+    password: 'password',
     database: 'SchoolDB'
 
 });
